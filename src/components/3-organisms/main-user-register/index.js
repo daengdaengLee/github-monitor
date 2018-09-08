a85fb43c1cc98ba9c6f6ac47e0c3c5d931b13dd5
daengdaenglee <gunho1020@gmail.com>
2018-09-09 00:44:39 +0900

    ENH: Connect register component to redux
    - after submit, add username to state

--- src/components/3-organisms/main-user-register/index.js
+++ src/components/3-organisms/main-user-register/index.js
@@ -1,80 +1,12 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import { Input, Button } from 'antd';
+import { connect } from 'react-redux';
+import { pushUser } from '../../../ducks/modules/users';
+import Presenter from './presenter';
 
-const Container = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  overflow: auto;
-  align-items: center;
-`;
+const mapDispatchToProps = dispatch => ({
+  onSubmitUser: username => dispatch(pushUser({ username })),
+});
 
-const Title = styled.h2`
-  height: 3rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const Form = styled.div`
-  height: 100%;
-  width: 80%;
-  padding: 3rem 0;
-  display: flex;
-  justify-content: center;
-  @media (min-width: 768px) {
-    width: 40rem;
-  }
-`;
-
-const SubmitButton = styled(Button)`
-  margin-left: 1rem;
-`;
-
-class MainUserRegister extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-    };
-    this._onChangeUsername = this._onChangeUsername.bind(this);
-    this._onClickSubmitButton = this._onClickSubmitButton.bind(this);
-  }
-  render() {
-    const { _onChangeUsername, _onClickSubmitButton } = this;
-    const { username } = this.state;
-    return (
-      <Container>
-        <Title>Search repos of user</Title>
-        <Form>
-          <Input
-            value={username}
-            placeholder="Input Github username"
-            onChange={_onChangeUsername}
-          />
-          <SubmitButton onClick={_onClickSubmitButton}>Submit</SubmitButton>
-        </Form>
-      </Container>
-    );
-  }
-
-  _onChangeUsername({ target: { value: username } }) {
-    this.setState({ username });
-  }
-
-  _onClickSubmitButton() {
-    const { onSubmitUser } = this.props;
-    const { username } = this.state;
-    onSubmitUser(username);
-    this.setState({ username: '' });
-  }
-}
-
-MainUserRegister.propTypes = {
-  onSubmitUser: PropTypes.func.isRequired,
-};
-
-export default MainUserRegister;
+export default connect(
+  null,
+  mapDispatchToProps,
+)(Presenter);

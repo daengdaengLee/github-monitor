66b207f24e12b31f9d51820ec11136855dee2dc5
daengdaenglee <gunho1020@gmail.com>
2018-09-09 10:26:34 +0900

    ENH: Input contents of repo infos

--- src/components/3-organisms/main-repo-dashboard/presenter.js
+++ src/components/3-organisms/main-repo-dashboard/presenter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Container = styled.div`
   width: 100%;
@@ -9,10 +9,48 @@ const Container = styled.div`
   overflow-y: auto;
   display: flex;
   flex-direction: column;
-  padding: 0 4rem;
+  padding: 0 2rem;
   @media (min-width: 768px) {
     padding: 0 10rem;
   }
+`;
+
+const Row = styled.div`
+  width: 100%;
+  height: ${props => props.height || '2rem'};
+  display: flex;
+  ${props =>
+    props.alignItems &&
+    css`
+      align-items: ${props.alignItems};
+    `};
+  ${props =>
+    props.justifyContent &&
+    css`
+      justify-content: ${props.justifyContent};
+    `};
+`;
+
+const Title = styled.h2`
+  width: 100%;
+  height: 2rem;
+  display: flex;
+`;
+
+const BlockA = styled.a`
+  width: ${props => props.width || '100%'};
+  height: 100%;
+  ${props =>
+    props.flexGrow !== undefined &&
+    css`
+      flex-grow: ${props.flexGrow};
+    `} display: flex;
+  align-items: center;
+  color: black;
+  &:hover {
+    color: black;
+  }
+  text-decoration: none !important;
 `;
 
 const MainRepoDashboard = ({ match, repos }) => {
@@ -21,11 +59,41 @@ const MainRepoDashboard = ({ match, repos }) => {
   const currentRepo = repoList.find(repo => `${repo.id}` === repoId);
   return !currentRepo ? null : (
     <Container>
-      {[...Array(1000)].map((v, i) => (
-        <div key={i} style={{ height: '2rem', minHeight: '2rem' }}>
-          {i}
-        </div>
-      ))}
+      <Title>
+        <BlockA href={currentRepo.html_url} target="_blank">
+          {currentRepo.name}
+        </BlockA>
+      </Title>
+      <Row>
+        <span
+          style={{
+            marginRight: '0.8rem',
+            display: 'inline-flex',
+            alignItems: 'center',
+          }}
+        >
+          owned by
+        </span>
+        <BlockA
+          href={currentRepo.owner.html_url}
+          target="_blank"
+          width="0"
+          flexGrow="1"
+        >
+          {currentRepo.owner.login}
+        </BlockA>
+      </Row>
+      <Row alignItems="center">
+        {currentRepo.description || 'No description'}
+      </Row>
+      <Row alignItems="center">
+        {`Watch: ${currentRepo.watchers_count}, Star: ${
+          currentRepo.stargazers_count
+        }`}
+      </Row>
+      <Row alignItems="center">
+        {`Language: ${currentRepo.language || 'No language'}`}
+      </Row>
     </Container>
   );
 };
